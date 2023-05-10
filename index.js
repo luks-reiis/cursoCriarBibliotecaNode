a5c929f1b48bc26f3aed234e5f40da795e87d265
Lucas Reis <luk.wik40@gmail.com>
2023-05-10 00:14:45 -0300

    Instalando dependência Chalk

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('olá mundo');
+import chalk from "chalk";
+
+console.log(chalk.blue("olá mundo"));
+
+console.log(
+  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do "
+);
+console.log(
+  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do "
+);
