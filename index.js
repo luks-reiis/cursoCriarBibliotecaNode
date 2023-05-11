dd6be741d21a5696960677858bd8d208136c2c74
Lucas Reis <111098018+luks-reiis@users.noreply.github.com>
2023-05-11 12:55:36 -0300

    Rescrevendo a função pegaArquivo com sintaxe de promises (then e catch)

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ function trataErro(erro) {
 
 function pegaArquivo(caminhoArquivo) {
   const encoding = 'utf-8';
-  fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      trataErro(erro);
-    }
-    console.log(chalk.green(texto));
-  })
+  fs.promises
+    .readFile(caminhoArquivo, encoding)
+    .then((texto) => {
+      console.log(chalk.green(texto))
+    })
+    .catch((erro) => {
+      trataErro(erro)
+    })
 }
+
+// function pegaArquivo(caminhoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
+//     if (erro) {
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
 
 pegaArquivo('./arquivos/texto.md');
