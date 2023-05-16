48bab9e2539be772e8956dca94d9781f2ba3c6ab
Lucas Reis <111098018+luks-reiis@users.noreply.github.com>
2023-05-16 13:03:42 -0300

    Criando arquivo de validacao.js

---
+++ src/http-validacao.js
@@ -0,0 +1,9 @@
+export default function listaValidada(listaDeLinks) {
+    return 'Entrou na função.';
+}
+
+function extraiLinks(arrLinks) {
+    arrLinks.map((objetoLink) => {
+        Object.values(objetoLink)
+    })
+}
